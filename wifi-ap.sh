97b4e6b3026225b22af12fa31f579ed37446f52e
Matthew Watkins <matthew.watkins@nokia.com>
2017-06-16 10:38:05 +0100

    [intel-nux-router] added wifi-ap.sh script

---
+++ wifi-ap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WIFI_DEV=wlp2s0
+ETH_DEV=enp0s25
+
+sudo yum -y install haveged iw hostapd
+if [ ! -d create_ap ]
+then
+	sudo git clone https://github.com/oblique/create_ap
+else
+	cd create_ap; sudo make install
+fi
+
+create_ap $WIFI_DEV $ETH_DEV NUC-Router nokia123
